4f967dec59042ddb294dd330d88ae9291030876d
tourdetravoy <dooganb@gmail.com>
2023-05-26 17:50:39 +0100

    Create ember.py

---
+++ ember.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import inspect
+import textwrap
+import pandas as pd
+import altair as alt
+
+from urllib.error import URLError
+
+
+def data_frame_demo():
+    @st.experimental_memo
+    def get_ember_data():
+        df = pd.read_csv("C:/Code/embersolar.csv")
+        return df.set_index("country_or_region")
+
+    try:
+        df = get_ember_data()
+        countries = st.multiselect(
+            "Choose country or region", list(df.index.unique()), ["China"]
+        )
+        if not countries:
+            st.error("Please select at least one region.")
+
+        else:
+            data = df.loc[countries]
+
+            chart = (
+                alt.Chart(data, width=1200, height=600)
+                .mark_bar(color='yellow')
+                .encode(
+                    x="date:T",
+                    y=alt.Y("generation_twh:Q", stack=None),
+                    tooltip=['date:T', 'generation_twh', 'generation_pct'],
+                )
+            )
+            st.altair_chart(chart)
+
+            st.write("### Solarpower Production (TWh)", data.sort_index())
+
+    except URLError as e:
+        st.error(
+            """
+            **This demo requires internet access.**
+            Connection error: %s
+        """
+            % e.reason
+        )
+
+st.set_page_config(layout="wide", page_title="Solarpower Production", page_icon="📊", initial_sidebar_state="collapsed")
+st.markdown("# Solarpower")
+st.write(
+"""This demo shows how to use `st.write` to visualize Pandas DataFrames.
+(Data courtesy of the [Ember Data Explorer](https://ember-climate.org/data/data-tools/data-explorer/).)"""
+)
+
+data_frame_demo()
